cad68582833c733d95408aa0e3aafcbff6a4de31
Paul Pernalon <paul.pernalon@gmail.com>
2022-02-24 18:40:22 +0100

    updating challenge

--- src/components/SharedChallenge.jsx
+++ src/components/SharedChallenge.jsx
@@ -1,13 +1,30 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { io } from "socket.io-client"
 import './SharedChallenge.css'
 
+// const SERVER_URL = "http://paul-pernalon.fr:3000"
+const SERVER_URL = "http://localhost:3000"
+
 const SharedChallenge = () => {
+    const [currentChallenge, setCurrentChallenge] = useState('') 
+    const [socket, setSocket] = useState(null)
+
+    useEffect(() => {
+        setSocket(io(`${SERVER_URL}`))
+    }, [])
+
+    useEffect(() => {
+        if (socket){
+            socket.on('connection', (ws) => setCurrentChallenge(ws.challenge))
+            socket.on('new.challenge', (ws) => setCurrentChallenge(ws.challenge))
+        }
+    }, [socket])
 
     return (
         <div className="SharedChallengeShadow">
             <div className="SharedChallenge">
                 <span>
-                    C'est le défi commun
+                    {currentChallenge}
                 </span>
             </div>
         </div>
