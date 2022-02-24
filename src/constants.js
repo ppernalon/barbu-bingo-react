ce33766a2515fd61d56653f17090bda49545fb31
Paul Pernalon <paul.pernalon@gmail.com>
2022-02-24 20:51:54 +0100

    constants file

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const constants = {
     // SERVER_URL : 'http://localhost:3000'
-    SERVER_URL : 'http://paul-pernalon.fr:3000'
+    SERVER_URL : 'https://paul-pernalon.fr:3000'
 }
 
 export default constants
